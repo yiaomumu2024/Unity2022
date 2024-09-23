e228a65d8a80837edcbe02763f701f96de5f1fac
yiaomumu <yiaomumu@qq.com>
2024-09-23 19:40:56 +0800

    Update Singleton.cs

--- Manager/Singleton.cs
+++ Manager/Singleton.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Scripting;
 
