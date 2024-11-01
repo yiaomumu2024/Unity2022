64981e637533ecd5cc0f2aed01e340d8f59f8d23
yiaomumu <yiaomumu@qq.com>
2024-11-01 18:31:48 +0800

    Update and rename EventMgr.cs to EventHelper.cs

--- Manager/EventHelper.cs
+++ Manager/EventHelper.cs
@@ -1,37 +1,18 @@
 using System;
 using System.Collections.Concurrent;
 using System.Threading;
-using UnityEngine;
 
-public class EventMgr : MonoBehaviour
+public static class EventHelper
 {
-    private static EventMgr instance;
-
-    public static EventMgr Ins
-    {
-        get
-        {
-            if (instance == null)
-            {
-                instance = FindObjectOfType<EventMgr>();
-                if (instance == null)
-                {
-                    GameObject obj = new GameObject("EvenManager");
-                    instance = obj.AddComponent<EventMgr>();
-                }
-            }
-            return instance;
-        }
-    }
 
     // 使用 ConcurrentDictionary 和 ConcurrentBag 确保线程安全。
-    private readonly ConcurrentDictionary<string, ConcurrentBag<Action<object[]>>> _eventDictionary = new ConcurrentDictionary<string, ConcurrentBag<Action<object[]>>>();
+    private static readonly ConcurrentDictionary<string, ConcurrentBag<Action<object[]>>> _eventDictionary = new ConcurrentDictionary<string, ConcurrentBag<Action<object[]>>>();
 
     /// <summary>
     /// 订阅事件
     /// </summary>
     /// <param name="eventName">事件名</param>
-    public void Subscribe(string eventName, Action<object[]> listener)
+    public static void Subscribe(string eventName, Action<object[]> listener)
     {
         var actions = _eventDictionary.GetOrAdd(eventName, _ => new ConcurrentBag<Action<object[]>>());
         actions.Add(listener);
@@ -41,7 +22,7 @@ public class EventMgr : MonoBehaviour
     /// 取消订阅事件
     /// </summary>
     /// <param name="eventName"></param>
-    public void Unsubscribe(string eventName, Action<object[]> listener)
+    public static void Unsubscribe(string eventName, Action<object[]> listener)
     {
         if (_eventDictionary.TryGetValue(eventName, out var actions))
         {
@@ -62,7 +43,7 @@ public class EventMgr : MonoBehaviour
     /// </summary>
     /// <param name="eventName">事件名</param>
     /// <param name="eventParams">事件参数</param>
-    public void Dispatch(string eventName, params object[] eventParams)
+    public static void Dispatch(string eventName, params object[] eventParams)
     {
         if (_eventDictionary.TryGetValue(eventName, out var actions))
         {
