2ce870571918196e8ebd3ea80ee41eff698b8bb2
yiaomumu <yiaomumu@qq.com>
2024-11-13 17:47:48 +0800

    Add files via upload

---
+++ ButtonExtension.cs
@@ -0,0 +1,174 @@
+﻿namespace Product.Script
+{
+    using UnityEngine;
+
+    using UnityEngine.Events;
+
+    using UnityEngine.EventSystems;
+
+    public class ButtonExtension : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler
+    {
+
+        public float pressDurationTime = 1;
+
+        public bool responseOnceByPress = false;
+
+        public float doubleClickIntervalTime = 0.5f;
+
+        [Header("点击的时候需要缩放的物体")]
+        public RectTransform btnRect;
+
+        public UnityEvent onDoubleClick;
+
+        public UnityEvent onPress;
+
+        public UnityEvent onClick;
+
+        public UnityEvent onClickUp;
+
+        public bool isDown = false;
+
+        public bool isUnscaleTime = false;
+
+        private bool isPress = false;
+
+        private float downTime = 0;
+
+        private float clickIntervalTime = 0;
+
+        private float clickTimes = 0;
+
+        public float pressedScaleRate = 0.8f;
+
+        private Vector3 pressedScale;
+
+        private Vector3 originalScale;
+
+        [Header("长按时多久触发一次事件")]
+        [SerializeField] private float longPressDuation = 0.1f;
+        [Header("长按多久开始加速（填-1为不加速），从开始长按算起（点击按钮1秒后开始计时）")]
+        [SerializeField] private float pressSpeedUpTime = -1f;
+        [Header("加速次数（-1为无限次加速）")]
+        [SerializeField] private float speedUpTimes = -1f;
+        [Header("加速速率（%）")]
+        [SerializeField] private int pressAddRate = 20;
+        private float checkSpeedUpTime;
+        private float pressClickDuation;
+        private float orilongPressDuation;
+        private float orispeedUpTimes;
+
+        protected void Awake()
+        {
+            btnRect.pivot = new Vector2(0.5f, 0.5f);
+            originalScale = btnRect.localScale;
+            pressedScale = originalScale * pressedScaleRate;
+
+            //第一次点击或者双击应该立即响应
+            clickIntervalTime = doubleClickIntervalTime;
+            pressClickDuation = longPressDuation;
+            orilongPressDuation = longPressDuation;
+            orispeedUpTimes = speedUpTimes;
+        }
+
+        void Update()
+        {
+            float delta = isUnscaleTime ? Time.unscaledDeltaTime : Time.deltaTime;
+            if (isDown)
+            {
+                if (responseOnceByPress && isPress)
+                {
+                    return;
+                }
+
+                downTime += delta;
+                pressClickDuation += delta;
+
+                if (downTime > pressDurationTime)
+                {
+                    isPress = true;
+                    CheckSpeedUp(delta);
+                    if (pressClickDuation >= longPressDuation)
+                    {  
+                        onPress.Invoke();
+                        pressClickDuation = 0;
+                    }
+
+                  
+                }
+            }
+
+            if (clickTimes >= 0.5)
+            {
+                clickIntervalTime += delta;
+                if (clickIntervalTime >= doubleClickIntervalTime)
+                {
+                    if (clickTimes >= 2)
+                    {
+                        onDoubleClick.Invoke();
+                    }
+                    else
+                    {
+                        onClick.Invoke();
+                    }
+                    clickTimes = 0;
+                    clickIntervalTime = 0;
+                }
+            }
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            isDown = true;
+            downTime = 0;
+            checkSpeedUpTime = 0;
+            btnRect.localScale = pressedScale;
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            isDown = false;
+            isPress = false;
+            btnRect.localScale = originalScale;
+            clickIntervalTime = doubleClickIntervalTime;
+            longPressDuation = orilongPressDuation;
+            speedUpTimes = orispeedUpTimes;
+            checkSpeedUpTime = 0;
+            onClickUp?.Invoke();
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            // isDown = false;
+            // isPress = false;
+            // btnRect.localScale = originalScale;
+            // clickIntervalTime = doubleClickIntervalTime;
+            // longPressDuation = orilongPressDuation;
+            // speedUpTimes = orispeedUpTimes;
+            // checkSpeedUpTime = 0;
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (!isPress)
+                //onClick.Invoke();
+                clickTimes += 0.5f;
+            else
+                isPress = false;
+        }
+
+        //检查长按加速
+        private void CheckSpeedUp(float delta)
+        {
+            checkSpeedUpTime += delta;
+
+            //达到加速条件
+            if (pressSpeedUpTime > 0 && checkSpeedUpTime >= pressSpeedUpTime && speedUpTimes != 0)
+            {
+                longPressDuation *= (1 - pressAddRate/100f);
+                speedUpTimes -= 1;
+                checkSpeedUpTime = 0;
+            }
+
+        }
+    }
+}
---
+++ LanguageHelper.cs
@@ -0,0 +1,184 @@
+﻿using GameFramework;
+using GameFramework.Localization;
+using Product.Configs;
+using Product.Script.Data;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityGameFramework.Runtime;
+
+namespace Product.Script.Tools
+{
+    /// <summary>
+    /// 文本的多语言
+    /// </summary>
+    public class MultiLanText
+    {
+
+        public bool isTextComponent = false;
+        public Text textComponent;
+        public TextMeshProUGUI tmpComponent;
+        public TMP_SubMeshUI subMeshUI;
+        public bool isBasicMaterial = false;
+        public string key;
+        public object[] args;
+
+        public void SetInfo(Text txt, string key, object[] args)
+        {
+            textComponent = txt;
+            this.key = key;
+            this.args = args;
+            isTextComponent = true;
+        }
+
+        public void SetInfo(TextMeshProUGUI txt, string key, object[] args)
+        {
+            tmpComponent = txt;
+            this.key = key;
+            this.args = args;
+            if (txt.fontMaterial.name == "BRLNSDB SDF Material (Instance)")
+                isBasicMaterial = true;
+            SetFont();
+        }
+
+        public void SetFont()
+        {
+            if (tmpComponent == null) return;
+            if (subMeshUI == null && tmpComponent.transform.childCount > 0)
+                subMeshUI = tmpComponent.transform.GetChild(0)?.GetComponent<TMP_SubMeshUI>();
+
+            if (isBasicMaterial)
+            {
+                tmpComponent.font = LanguageHelper.tmpFontAsset;
+                return;
+            }
+            if (subMeshUI != null)
+                subMeshUI.fontAsset = LanguageHelper.tmpFontAsset;
+            else
+                if (TMP_Settings.defaultFontAsset != LanguageHelper.tmpFontAsset)
+                Log.Warning($"LanguageHelper 设置 {tmpComponent.name} 多语言失败，没有找到FallBackAsset : {LanguageHelper.tmpFontAsset}");
+        }
+    }
+
+    /// <summary>
+    /// 多语言工具辅助类
+    /// </summary>
+    public static class LanguageHelper
+    {
+        // 缓存文本组件，用于热更新语言
+        private static Dictionary<int, MultiLanText> textDic = new Dictionary<int, MultiLanText>();
+
+        public static Language systemLanguage => GameFrameworkEntry.GetModule<ILocalizationManager>().SystemLanguage;
+
+        public static Language CurLan;
+        public static TMP_FontAsset tmpFontAsset;
+
+        private static void SetLanguageFonts()
+        {
+            if (CurLan == Language.ChineseSimplified || CurLan == Language.ChineseTraditional || CurLan == Language.Japanese)
+            {
+                tmpFontAsset = TMP_Settings.fallbackFontAssets[0];
+            }
+            else if (CurLan == Language.Korean)
+            {
+                tmpFontAsset = TMP_Settings.fallbackFontAssets[1];
+            }
+            else
+            {
+                tmpFontAsset = TMP_Settings.defaultFontAsset;
+            }
+        }
+
+        public static string GetString(string maybeKey)
+        {
+            if (string.IsNullOrEmpty(maybeKey)) { return string.Empty; }
+            CurLan = GameEntry.Base.EditorLanguage;
+            string val;
+            if (CurLan == Language.Unspecified)
+                val = GameEntry.Data.GetData<LanguageData>().GetString(maybeKey, systemLanguage);
+            else
+                val = GameEntry.Data.GetData<LanguageData>().GetString(maybeKey, CurLan);
+            if (string.IsNullOrEmpty(val))
+                return maybeKey;
+            else
+            {
+                val = val.Replace("\\n", "\n");
+                if (CurLan == Language.ChineseSimplified || CurLan == Language.ChineseTraditional)
+                    val = val.Replace(" ", "\u00A0");
+                return val;
+            }
+
+        }
+
+        public static void SetText(Text textComponent, string maybeKey, params object[] args)
+        {
+            if (textComponent == null) { return; }
+            string val = GetString(maybeKey);
+            if (!val.Equals(maybeKey))
+            {
+                int uuid = textComponent.GetInstanceID();
+                MultiLanText multiLanText;
+                textDic.TryGetValue(uuid, out multiLanText);
+                if (multiLanText == null)
+                {
+                    multiLanText = new MultiLanText();
+                    textDic.Add(uuid, multiLanText);
+                }
+                multiLanText.SetInfo(textComponent, maybeKey, args);
+            }
+            textComponent.text = string.Format(val, args);
+        }
+
+        public static void SetText(TextMeshProUGUI textComponent, string maybeKey, params object[] args)
+        {
+            if (textComponent == null) { return; }
+            string val = GetString(maybeKey);
+            if (!val.Equals(maybeKey))
+            {
+                int uuid = textComponent.GetInstanceID();
+                MultiLanText multiLanText;
+                textDic.TryGetValue(uuid, out multiLanText);
+                if (multiLanText == null)
+                {
+                    multiLanText = new MultiLanText();
+                    textDic.Add(uuid, multiLanText);
+                }
+                multiLanText.SetInfo(textComponent, maybeKey, args);
+            }
+            try
+            {
+                textComponent.text = string.Format(GetString(maybeKey), args);
+            }
+            catch (Exception exception)
+            {
+                Debug.LogWarning("Multilanguage Error : exception" + exception.ToSafeString());
+                textComponent.text = maybeKey;
+            }
+        }
+
+        /// <summary>
+        /// 语言环境更改时，动态更新缓存中的文本
+        /// </summary>
+        public static void UpdateTextView()
+        {
+            if (CurLan == GameEntry.Base.EditorLanguage) return;
+            CurLan = GameEntry.Base.EditorLanguage;
+            SetLanguageFonts();
+            foreach (var item in textDic)
+            {
+                MultiLanText text = item.Value;
+                if (text.isTextComponent)
+                    SetText(text.textComponent, text.key, text.args);
+                else
+                {
+                    text.SetFont();
+                    SetText(text.tmpComponent, text.key, text.args);
+                }
+            }
+        }
+    }
+}
